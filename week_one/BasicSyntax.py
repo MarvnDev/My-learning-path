5528531d812a8aeb2f4bbd1af3eb69f740c23811
Marvin Ramos <mr487223@gmail.com>
2022-11-06 13:58:22 -0600

    entendiendo sintaxis

--- week_one/BasicSyntax.py
+++ week_one/BasicSyntax.py
@@ -1,1 +1,23 @@
-print('hello world')
+# creando variables
+x = 1
+z = float(12)
+info = '''Hola
+mi nombre es lola
+jajaja
+:)'''
+
+array = "hello, world!"
+
+for word in "sapo perro miado":
+    print(word)
+
+print(array[1])
+# TIPOS DE DATOS
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# None Type:	NoneType
